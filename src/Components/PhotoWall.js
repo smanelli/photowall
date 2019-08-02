b8d0795bface5f7fc695125ea1b9e3d288698b4f
Stefano Manelli <smanelli@gmail.com>
2019-08-02 10:09:35 +0200

    Fine React

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Photo from './Photo'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
 function PhotoWall(props) {
-    return  <div className='photo-grid'>  
-                {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+    return  <div>
+                <Link className='addIcon' onClick={props.onNavigate} to="/AddPhoto"><br/></Link>
+                <div className='photo-grid'>  
+                    {props.posts
+                        .sort(function(x,y) {
+                            return y.id -x.id
+                        })
+                        .map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+                </div>
             </div>
 }
 
-
+PhotoWall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
 
 export default PhotoWall
